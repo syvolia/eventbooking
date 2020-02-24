595b69ff8545297ae9d8599d2b465718b15af5dd
syvolia mary atieno <syvoliamary@gmail.com>
2020-02-24 23:01:10 +0300

    Update app.js

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ mongoose
     `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-qocko.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`
   )
   .then(() => {
-    app.listen(8000);
+    app.listen(process.env.PORT || 8000);
   })
   .catch(err => {
     console.log(err);
